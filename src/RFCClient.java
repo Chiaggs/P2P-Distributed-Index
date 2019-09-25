8436ace2c0f4dfcaffbfb7f105c27dce88bbaa39
Chiaggs <chiaggs@gmail.com>
2019-09-25 19:00:33 -0400

    Added project files

---
+++ src/RFCClient.java
@@ -0,0 +1,11 @@
+public class RFCClient extends Thread{
+    int portNumber;
+    RFCClient(int portNumber){
+        this.portNumber = portNumber;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
