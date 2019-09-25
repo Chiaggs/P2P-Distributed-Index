dcd5ad76cc9d7b5de0ebbe8d8b9ed01e2b1b0cea
SHRIJEET <shrijeetj@gmail.com>
2019-09-25 18:32:58 -0400

    RS commit initial

--- src/RegistrationServer.java
+++ src/RegistrationServer.java
@@ -1,5 +1,35 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class RegistrationServer {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Registration server started!");
+        ServerSocket rsServerSocket = null;
+        Socket rsSocket = null;
+
+        try{
+
+            rsServerSocket = new ServerSocket(65423, 5);
+        }
+        catch(IOException e) {
+            System.out.print("RS Server socket could not be started!");
+            e.printStackTrace();
+        }
+
+        try {
+            while(true) {
+                rsSocket = rsServerSocket.accept();
+                System.out.print("Connected with Peer!"+rsSocket.getInetAddress()+ rsSocket.getPort());
+
+            }
+
+        }
+        catch(Exception e) {
+            System.out.print("RS Error while accepting clients!");
+            e.printStackTrace();;
+        }
+
     }
+
 }
