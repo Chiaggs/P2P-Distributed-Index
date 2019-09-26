e334d13ac23f5dac36d5ec6cb5be29ac69ecf5b9
SHRIJEET <shrijeetj@gmail.com>
2019-09-25 20:12:14 -0400

    Commit 6, RS Server Initial REG 0 1

--- src/RFCClientHandler.java
+++ src/RFCClientHandler.java
@@ -1,3 +1,5 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.Socket;
 import java.util.Date;
 import java.util.LinkedList;
@@ -16,6 +18,7 @@ public class RFCClientHandler extends Thread{
 
        public ClientInfo() {
            this.ttl = 7200;
+           this.regCount = 0;
        }
     }
 
@@ -28,11 +31,63 @@ public class RFCClientHandler extends Thread{
     @Override
     public void run() {
         try {
+            DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+            String reqString = dis.readUTF();
+            String []reqStringArr = reqString.split(" ");
+
             ClientInfo clientObj = new ClientInfo();
             clientObj.hostName = clientSocket.getInetAddress().toString().trim();
             clientObj.cookie = cookie++;
             clientObj.flag = true;
-            clientObj.rfcServerPort = 12345;
+            clientObj.rfcServerPort = Integer.parseInt(reqStringArr[2].trim());
+            clientObj.regCount+=1;
+            clientObj.recentRegDateTime = new Date(System.currentTimeMillis());
+
+            Boolean existingClient = false;
+            if(reqStringArr[0].trim().equals("REG") && reqStringArr[1].trim().equals("1")) {
+                for(ClientInfo ci : peerList) {
+                    if(ci.hostName.equals(clientObj.hostName) && ci.rfcServerPort.equals(clientObj.rfcServerPort)) {
+                        ci.flag = true;
+                        ci.cookie = clientObj.cookie;
+                        ci.ttl = clientObj.ttl;
+                        ci.regCount+=1;
+                        existingClient = true;
+                        break;
+                    }
+                }
+
+                if(existingClient == false) {
+                    peerList.add(clientObj);
+                }
+                //responseMessage="RRG "+"OK"+"************"+"Registered";
+                DataOutputStream responseToClient=new DataOutputStream(clientSocket.getOutputStream());
+                responseToClient.writeUTF("RRG "+"OK"+"************"+"Registered");
+
+            }
+            else if(reqStringArr[0].trim().equals("REG") && reqStringArr[1].trim().equals("0") ) {
+                for(ClientInfo ci : peerList) {
+                    if(ci.hostName.equals(clientObj.hostName) && ci.rfcServerPort.equals(clientObj.rfcServerPort)) {
+                        ci.flag = false;
+                        ci.cookie = clientObj.cookie;
+                        ci.ttl = 0;
+                        //ci.regCount+=1;
+                        existingClient = true;
+                        break;
+                    }
+                }
+
+                if(existingClient == false) {
+                    //peerList.add(clientObj);
+                    System.out.print("Client is not registered! Cannot Unregister!");
+                }
+                DataOutputStream responseToClient=new DataOutputStream(clientSocket.getOutputStream());
+                responseToClient.writeUTF("\"RRG \"+\"EX\"+\"************\"+\"Exit\"");
+
+            }
+            System.out.print("Peer List:\n");
+            for(ClientInfo ci : peerList) {
+                System.out.print(ci.toString());
+            }
 
         }
         catch(Exception e) {
