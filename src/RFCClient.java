9440b42f5ae4d58f31b85d535826396c432901c4
SHRIJEET <shrijeetj@gmail.com>
2019-09-28 17:45:27 -0400

    Updated messages for GET RFC-Index and GET RFC

--- src/RFCClient.java
+++ src/RFCClient.java
@@ -3,6 +3,8 @@ import com.google.gson.reflect.TypeToken;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -11,9 +13,13 @@ import java.util.List;
 import java.util.Scanner;
 
 public class RFCClient extends Thread {
+    static final Integer N = 60;
     int portNumber;
     int cookie;
     static ArrayList<ClientInfo> peerList = new ArrayList<>();
+    static LinkedList<RFCIndex> RFCIndexList = new LinkedList<>();
+    static Boolean[] RFCIndexCheck = new Boolean[N];
+
 
     RFCClient(int portNumber) {
         this.portNumber = portNumber;
@@ -22,6 +28,7 @@ public class RFCClient extends Thread {
 
     @Override
     public void run() {
+        initializeRFCIndex();
         Scanner sc = new Scanner(System.in);
         System.out.println("Client has been started");
         while (true) {
@@ -45,6 +52,26 @@ public class RFCClient extends Thread {
                 }
             } catch (Exception e) {
                 System.out.println("Exception Occured: " + e);
+            }
+        }
+    }
+
+    public void initializeRFCIndex() {
+        File temp = null;
+        for(int i=0; i<N; i++) {
+            try {
+                temp = new File(i +".txt");
+                if(temp.exists()) {
+                    RFCIndexCheck[i] = true;
+                    //RFCIndexList.add(new RFCIndex(i,));
+                }
+                else {
+                    RFCIndexCheck[i] = false;
+                }
+
+            }
+            catch(Exception e) {
+                e.printStackTrace();
             }
         }
     }
@@ -127,11 +154,11 @@ public class RFCClient extends Thread {
         //Socket s = new Socket(rsIP, 65423);
         System.out.println("Socket created, sending outputstream");
 
-        DataOutputStream outToRS = new DataOutputStream(s.getOutputStream());
+        //DataOutputStream outToRS = new DataOutputStream(s.getOutputStream());
         //outToRS.writeUTF(requestMessage);
         System.out.println("Request message successfully sent");
 
-        //DataInputStream inFromRS = new DataInputStream((s.getInputStream()));
+        DataInputStream inFromRS = new DataInputStream((s.getInputStream()));
         //String responseMessage = inFromRS.readUTF();
         //System.out.println("Response received from server is: " + responseMessage);
     }
